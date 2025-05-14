cc66baf29a57aa085cbb66f27b3621d307d8e1c1
sarangaDananjana <dananjanasaranga@gmail.com>
2025-05-14 13:55:51 +0530

    urls.py update

--- machine/urls.py
+++ machine/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import login_conductor, route_sections
 
 urlpatterns = [
-    path('api/machine/login/', login_conductor, name='machine_login'),
-    path('api/machine/<int:route_id>/', route_sections, name='route_sections'),
+    path('login/', login_conductor, name='machine_login'),
+    path('<int:route_id>/', route_sections, name='route_sections'),
 ]
