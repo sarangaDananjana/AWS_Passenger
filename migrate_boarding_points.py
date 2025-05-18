263ac3004eaaf55aa347d7e044937f2ed10b4172
sarangaDananjana <dananjanasaranga@gmail.com>
2025-05-18 11:23:10 +0530

    migrate boerding points erros fixed

--- migrate_boarding_points.py
+++ migrate_boarding_points.py
@@ -4,11 +4,11 @@ import django
 from pymongo import MongoClient
 
 # 1. Bootstrap Django
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project.settings")  # ← change to your settings module
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "passenger.settings")  # ← change to your settings module
 django.setup()
 
 # 2. Import your model
-from your_app.models import BoardingPoint  # ← change your_app to your Django app name
+from busstops.models import BoardingPoint  # ← change your_app to your Django app name
 
 # 3. Use your MongoDB URI
 MONGO_URI = "mongodb+srv://dananjanasaranga:A0763003258z@cluster0.fkjv2k4.mongodb.net/Passenger?retryWrites=true&w=majority&appName=Cluster0"
